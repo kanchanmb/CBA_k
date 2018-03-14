1ed244d67bb550731e69de51c692edd1dbcca5a6
kanchanmb <37361616+kanchanmb@users.noreply.github.com>
2018-03-14 17:32:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int a = 5, b = 10, temp
+    int a = 5, b = 10, temp;
 
     cout << "Before swapping." << endl;
     cout << "a = " << a << ", b = " << b << endl;
@@ -16,5 +16,5 @@ int main()
     cout << "a = " << a << ", b = " << b << endl;
 
     return 0;
-	}
+	
 }
